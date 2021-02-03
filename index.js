851c204db159742b7786cd355b1e800fa990583b
LauraToddCode <laura20todd@gmail.com>
2021-02-03 11:46:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-$( "div.menuDropbtnDesktop" ).click(function() { var x = document.getElementById("navList");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-        $( "ul" ).slideToggle( "slow" );
-      });
+$("div.menuDropbtnDesktop").click(function() { 
+    var x = document.getElementById("navList");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    $("ul").slideToggle("slow");
+});
 
-      function myMenuDropdown() {
-        document.getElementById("menuDropbtnDesktop").classList.toggle("show");
-        }
+function myMenuDropdown() {
+    document.getElementById("menuDropbtnDesktop").classList.toggle("show");
+}
+
+        
